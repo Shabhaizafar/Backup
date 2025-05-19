c0c3bb99d667ce31509346a69cb4507c3442cdc0
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-19 13:12:56 +0530

    Op

---
+++ Operators.py
@@ -0,0 +1,118 @@
+'''
+
+### **Arithmetic Operators**
+- `+`
+- `-`
+- `*`
+- `/`
+- `%`
+- `**`
+- `//`
+'''
+# n1 = 12
+# n2 = 2
+# print("Value of N1 : ",n1)
+# print("Value of N2 : ",n2)
+# print("Addition  : ",n1+n2)   #  15
+# print("Subtraction  : ",n1-n2) # 9
+# print("Division  : ",n1/n2) # 4.0    // float
+# print("Multi  : ",n1*n2)
+# print("Modulo  : ",n1%n2)
+
+# print("Floor Division : ",n1//n2) # 4
+
+# print("Power : ",n1**n2) 
+
+
+'''
+
+### **Assignment Operators**
+- `=`
+- `+=`
+- `-=`
+- `*=`
+- `/=`
+- `%=`
+- `**=`
+- `//=`
+
+'''
+
+# n1 = 12
+# n2 = 2
+
+# n1+=n2  # n1 = n1+n2
+# n1/=n2 # n1 = n1/n2 
+# n1//=n2 
+# n1**=n2 
+# print(n1)
+
+
+'''
+
+### **Comparison Operators**
+Bool  : True / False
+- `==`
+- `!=`
+- `>`
+- `<`
+- `>=`
+- `<=`
+
+'''
+
+# n1 = 12
+# n2 = 2
+# print(n1==n2)
+# print(n1!=n2)
+
+
+'''
+
+### **Logical Operators**
+- `and`
+- `or`
+- `not`
+
+'''
+
+# print(12<13 and 13!=12)
+
+
+
+'''
+
+### **Membership Operators**
+- `in`
+- `not in`
+
+---
+
+### **Identity Operators**
+- `is`
+- `is not`
+
+'''
+# print([11,12,13,14,15])
+# print(133 in [11,12,13,14,15])
+# print(133 not in [11,12,13,14,15])
+
+n1 = 12
+n2 = 12 
+print(n1 is n2)
+n1+=1
+print(n1 is n2)
+
+
+'''
+
+### **Bitwise Operators**
+- `&`
+- `|`
+- `^`
+
+- `~`
+- `<<`
+- `>>`
+
+'''
